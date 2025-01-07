2ec6d6fb76c23bf04a0ca472850424f3220db436
OSEAS TEIXEIRA <oseas.teixeira@ufvjm.edu.br>
2025-01-07 21:48:35 +0000

    Main * feat: init gitpod config
    * fix: pin numpy, replace poetry with pip and requirements file, add .gitignore
    
    * fix: use main.py as the entrypoint
    
    * Update .gitpod.yml
    
    Co-authored-by: Shaun Hamilton <shauhami020@gmail.com>
    
    ---------
    
    Co-authored-by: Shaun Hamilton <shauhami020@gmail.com>
    
    Checklist:
    
    <!-- Please follow this checklist and put an x in each of the boxes, like this: [x]. It will ensure that our team takes your pull request seriously. -->
    
    - [ ] I have read [freeCodeCamp's contribution guidelines](https://contribute.freecodecamp.org).
    - [ ] My pull request has a [descriptive title](https://contribute.freecodecamp.org/#/how-to-open-a-pull-request?id=prepare-a-good-pr-title) (**not** a vague title like `Update index.md`)
    
    <!--If your pull request closes a GitHub issue, replace the XXXXX below with the issue number.-->
    
    Closes #XXXXX
    
    <!-- Feel free to add any additional description of changes below this line -->

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,47 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # Convertendo a lista em uma matriz 3x3
+    arr = np.array(list).reshape(3, 3)
+    
+    # Calculando os valores requisitados
+    calculations = {
+        'mean': [
+            arr.mean(axis=0).tolist(),  # média das colunas
+            arr.mean(axis=1).tolist(),  # média das linhas
+            arr.mean().tolist()         # média da matriz nivelada
+        ],
+        'variance': [
+            arr.var(axis=0).tolist(),   # variância das colunas
+            arr.var(axis=1).tolist(),   # variância das linhas
+            arr.var().tolist()          # variância da matriz nivelada
+        ],
+        'standard deviation': [
+            arr.std(axis=0).tolist(),   # desvio-padrão das colunas
+            arr.std(axis=1).tolist(),   # desvio-padrão das linhas
+            arr.std().tolist()          # desvio-padrão da matriz nivelada
+        ],
+        'max': [
+            arr.max(axis=0).tolist(),   # máximo das colunas
+            arr.max(axis=1).tolist(),   # máximo das linhas
+            arr.max().tolist()          # máximo da matriz nivelada
+        ],
+        'min': [
+            arr.min(axis=0).tolist(),   # mínimo das colunas
+            arr.min(axis=1).tolist(),   # mínimo das linhas
+            arr.min().tolist()          # mínimo da matriz nivelada
+        ],
+        'sum': [
+            arr.sum(axis=0).tolist(),   # soma das colunas
+            arr.sum(axis=1).tolist(),   # soma das linhas
+            arr.sum().tolist()          # soma da matriz nivelada
+        ]
+    }
+    
     return calculations
+
+
+
