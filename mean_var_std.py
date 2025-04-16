ac29f06b9984f1b0707cf7c141b6daf716529874
Marutheisweran <3snetworksolutions@gmail.com>
2025-04-16 13:09:34 +0800

    mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -3,6 +3,13 @@ import numpy as np
 def calculate(list):
 
 
-
+{
+  'mean': [axis1, axis2, flattened],
+  'variance': [axis1, axis2, flattened],
+  'standard deviation': [axis1, axis2, flattened],
+  'max': [axis1, axis2, flattened],
+  'min': [axis1, axis2, flattened],
+  'sum': [axis1, axis2, flattened]
+}
 
     return calculations
