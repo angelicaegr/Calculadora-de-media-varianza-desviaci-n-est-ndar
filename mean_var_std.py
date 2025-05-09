4fac5053d4047a61e3221ddbc95d88634b870979
JoeZhao262 <zhaojoe262@gmail.com>
2025-05-09 09:27:13 +0800

    Update mean_var_std.py
    first edition

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,41 @@
 import numpy as np
 
-def calculate(list):
+def calculate(input_list):
+    if len(input_list)<9:
+        raise ValueError( "List must contain nine numbers.")
+    reshaped=np.reshape(input_list,(3,3))
+    result={
+        'mean': [
+            np.mean(reshaped,axis=0).to_list(),
+            np.mean(reshaped,axis=1).to_list(),
+            np.mean(reshaped).item()
+        ],
+        'variance': [
+            np.var(reshaped,axis=0).to_list(),
+            np.var(reshaped,axis=1).to_list(),
+            np.var(reshaped).item()
+        ],
+    'standard deviation': [
+            np.std(reshaped,axis=0).to_list(),
+            np.std(reshaped,axis=1).to_list(),
+            np.std(reshaped).item()
+        ],
+    'max': [
+            np.max(reshaped,axis=0).to_list(),
+            np.max(reshaped,axis=1).to_list(),
+            np.max(reshaped).item()
+        ],
+    'min': [
+            np.min(reshaped, axis=0).tolist(),
+            np.min(reshaped, axis=1).tolist(),
+            np.min(reshaped).item()
+        ],
+        'sum': [
+            np.sum(reshaped, axis=0).tolist(),
+            np.sum(reshaped, axis=1).tolist(),
+            np.sumreshaped).item()
+        ]
+    }
 
-
-
-
-    return calculations
+        
+    return result
