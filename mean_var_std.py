9c460c4773ad74b6c49507fbc8ac96b44e5d11ac
angelicaegr <angelicagomez2611@gmail.com>
2025-09-19 16:50:29 -0400

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -9,13 +9,22 @@ mean_rows = [ls[[0,1,2]].mean(), ls[[3,4,5]].mean(), ls[[6,7,8]].mean()]
 mean_colums =  [ls[[0,3,6]].mean(), ls[[1,4,7]].mean(), ls[[2,5,8]].mean()]
 
 var_rows = [ls[[0,1,2]].var(), ls[[3,4,5]].var(), ls[[6,7,8]].var()]
-mean_colums =  [ls[[0,3,6]].var(), ls[[1,4,7]].var(), ls[[2,5,8]].var.()]
+var_colums =  [ls[[0,3,6]].var(), ls[[1,4,7]].var(), ls[[2,5,8]].var.()]
+
+std_rows = [ls[[0,1,2]].std(), ls[[3,4,5]].std(), ls[[6,7,8]].std()]
+std_colums =  [ls[[0,3,6]].std(), ls[[1,4,7]].std(), ls[[2,5,8]].std.()]
+
+max_rows = [ls[[0,1,2]].max(), ls[[3,4,5]].max(), ls[[6,7,8]].max()]
+max_colums =  [ls[[0,3,6]].max(), ls[[1,4,7]].max(), ls[[2,5,8]].max.()]
+
+min_rows = [ls[[0,1,2]].min(), ls[[3,4,5]].min(), ls[[6,7,8]].min()]
+min_colums =  [ls[[0,3,6]].min(), ls[[1,4,7]].min(), ls[[2,5,8]].min.()]
 
 return(  
   'mean': [mean_colums, mean_arrowa, ls.mean()],
-  'variance': [axis1, axis2, flattened],
-  'standard deviation': [axis1, axis2, flattened],
-  'max': [axis1, axis2, flattened],
+  'variance': [var_colums, var_rows, ls.var()],
+  'standard deviation':[std_colums, std_rows, ls.var()],
+  'max': [max_colums, max_rows, ls.max()],
   'min': [axis1, axis2, flattened],
   'sum': [axis1, axis2, flattened]
 
