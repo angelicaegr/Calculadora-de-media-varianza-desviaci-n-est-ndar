9667e37464952d4b2cb44447ff0a39610e9543ee
angelicaegr <angelicagomez2611@gmail.com>
2025-09-19 16:43:48 -0400

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,7 +6,10 @@ ls = np.array(list)
 print(ls)
 
 mean_rows = [ls[[0,1,2]].mean(), ls[[3,4,5]].mean(), ls[[6,7,8]].mean()]
-mean_colums =  [ls[[0,4,6]].mean(), ls[[1,3,5]].mean(), ls[[2,7,8]].mean()]
+mean_colums =  [ls[[0,3,6]].mean(), ls[[1,4,7]].mean(), ls[[2,5,8]].mean()]
+
+var_rows = [ls[[0,1,2]].var(), ls[[3,4,5]].var(), ls[[6,7,8]].var()]
+mean_colums =  [ls[[0,3,6]].var(), ls[[1,4,7]].var(), ls[[2,5,8]].var.()]
 
 return(  
   'mean': [mean_colums, mean_arrowa, ls.mean()],
