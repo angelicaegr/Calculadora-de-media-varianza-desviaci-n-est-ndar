4fa808b16b8b9a22fa6b1e5a69a75c2a50b22fad
angelicaegr <angelicagomez2611@gmail.com>
2025-09-19 22:05:14 -0400

    goo

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,44 +1,34 @@
 import numpy as np
 
-def calculate(list):
-    if len(list) != 9:
-        raise ValueError("List must contain nine numbers.")
-    
-    matrix = np.array(list).reshape(3, 3)
+def calculate(ls):
 
-    print(matrix)
+  raise valueError('list must contain nine numbers,')
+print(ls)
 
-    calculations = {
-        'mean': [
-            matrix.mean(0).tolist(),  
-            matrix.mean(1).tolist(),  
-            float(matrix.mean())           
-        ],
-        'variance': [
-            matrix.var(axis=0).tolist(),   
-            matrix.var(axis=1).tolist(),   
-            float(matrix.var())            
-        ],
-        'standard deviation': [
-            matrix.std(axis=0).tolist(),   
-            matrix.std(axis=1).tolist(),   
-            float(matrix.std())            
-        ],
-        'max': [
-            matrix.max(axis=0).tolist(),   
-            matrix.max(axis=1).tolist(),   
-            int(matrix.max())              
-        ],
-        'min': [
-            matrix.min(axis=0).tolist(),   
-            matrix.min(axis=1).tolist(),   
-            int(matrix.min())              
-        ],
-        'sum': [
-            matrix.sum(axis=0).tolist(),  
-            matrix.sum(axis=1).tolist(),   
-            int(matrix.sum())              
-        ]
-    }
+ls = np.array(ls)
+print(ls)
 
-    return calculations
+mean_rows = [ls[[0,1,2]].mean(), ls[[3,4,5]].mean(), ls[[6,7,8]].mean()]
+mean_colums =  [ls[[0,3,6]].mean(), ls[[1,4,7]].mean(), ls[[2,5,8]].mean()]
+
+var_rows = [ls[[0,1,2]].var(), ls[[3,4,5]].var(), ls[[6,7,8]].var()]
+var_colums =  [ls[[0,3,6]].var(), ls[[1,4,7]].var(), ls[[2,5,8]].var.()]
+
+std_rows = [ls[[0,1,2]].std(), ls[[3,4,5]].std(), ls[[6,7,8]].std()]
+std_colums =  [ls[[0,3,6]].std(), ls[[1,4,7]].std(), ls[[2,5,8]].std.()]
+
+max_rows = [ls[[0,1,2]].max(), ls[[3,4,5]].max(), ls[[6,7,8]].max()]
+max_colums =  [ls[[0,3,6]].max(), ls[[1,4,7]].max(), ls[[2,5,8]].max.()]
+
+min_rows = [ls[[0,1,2]].min(), ls[[3,4,5]].min(), ls[[6,7,8]].min()]
+min_colums =  [ls[[0,3,6]].min(), ls[[1,4,7]].min(), ls[[2,5,8]].min.()]
+
+return(  
+  'mean': [mean_colums, mean_arrowa, ls.mean()],
+  'variance': [var_colums, var_rows, ls.var()],
+  'standard deviation':[std_colums, std_rows, ls.var()],
+  'max': [max_colums, max_rows, ls.max()],
+  'min': [min_colums, min_rows, ls.min()],
+  'sum': [sum_colums, sum_rows, ls.sum()]
+
+)
